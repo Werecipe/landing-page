c50106b3abc34bec7cfadee5397971f34bac428c
Raul-Octavina Albu <alburaul@gmil.com>
2021-04-21 15:11:24 +0200

    parallax changed on phone

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,11 +1,28 @@
 
+var width = window.innerWidth;
 
 var image = document.getElementsByClassName('image');
-new simpleParallax(image, {
-    scale: 2,
-    orientation: "down",
-    maxTransition: 75,
-    overflow: true,
-    delay: 0.3,
-});
+
+
+
+function parallaxDown() {
+    new simpleParallax(image, {
+        scale: 2,
+        orientation: "down",
+        maxTransition: 75,
+        overflow: true,
+        delay: 0.3,
+    });
+}
+    
+function parallaxRight() {
+    new simpleParallax(image, {
+        scale: 1.3,
+        orientation: "right",
+        maxTransition: 75,
+        overflow: true,
+        delay: 0.3,
+    });
+}
+width < 768 ? parallaxRight() : parallaxDown();
 
