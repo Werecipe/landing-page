24f41a10e7c1a0b0582b238a3867781699184d87
Raul-Octavina Albu <alburaul@gmil.com>
2021-05-13 11:53:09 +0200

    ongoarding video added?

---
+++ scripts/lottieControls.js
@@ -0,0 +1,16 @@
+
+
+// const onboardingVideo = document.querySelector("lottie-player");
+const onboardingVideo = document.querySelector("#onboarding");
+
+onboardingVideo.addEventListener('click', e => {
+  onboardingVideo.toggleAttribute("controls", "true");
+});
+
+
+
+
+
+
+
+
